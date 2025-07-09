6238b1fd12fe083a14c17a5296fc685a967c1d1b
Shashika Theekshana <shashikatheekshana@icloud.com>
2025-07-09 12:57:45 +0530

    Initial Commit

---
+++ CODECRAFT_AD_03( Stopwatch App )/CODECRAFT_AD_03__Stopwatch_App__App.swift
@@ -0,0 +1,17 @@
+//
+//  CODECRAFT_AD_03__Stopwatch_App__App.swift
+//  CODECRAFT_AD_03( Stopwatch App )
+//
+//  Created by shashika theekshana on BE 2568-07-09.
+//
+
+import SwiftUI
+
+@main
+struct CODECRAFT_AD_03__Stopwatch_App__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CODECRAFT_AD_03( Stopwatch App )/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CODECRAFT_AD_03( Stopwatch App )
+//
+//  Created by shashika theekshana on BE 2568-07-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
